355d2a2e339c5ef1ad553ff75598cd42335fc4a6
sub77 <sub77@ymail.com>
2015-06-08 20:56:30 +0200

    build in studio

--- src/net/sourceforge/opencamera/CameraController/CameraControllerException.java
+++
@@ -1,6 +0,0 @@
-package net.sourceforge.opencamera.CameraController;
-
-public class CameraControllerException extends Exception {
-	private static final long serialVersionUID = 7904697847749213106L;
-
-};
--- src/net/sourceforge/opencamera/MyDebug.java
+++
@@ -1,5 +0,0 @@
-package net.sourceforge.opencamera;
-
-public class MyDebug {
-	public static final boolean LOG = false;
-}
